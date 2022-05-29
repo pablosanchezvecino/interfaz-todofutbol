e09c3158e8251eca88fdf3e5bced48ac59d8b310
JaviZG <javizaygal@gmail.com>
2022-05-29 18:57:24 +0200

    login v3

--- login.js
+++ login.js
@@ -1,23 +1,57 @@
 const boton = document.getElementById("enviar");
 const formulario = document.getElementById("formulario");
 const error = document.getElementById("error");
-import {findByUsernameAndPassword} from './mongo.js';
+const { MongoClient } = require('mongodb');
+//import {findByUsernameAndPassword} from './mongo.js';
 
-const uri = "mongodb+srv://pablo:1234@cluster0.m2uj4.mongodb.net/test?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
+
+
+async function main(){
+    const uri = "mongodb+srv://pablo:1234@cluster0.m2uj4.mongodb.net/test?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+    console.log("prueba");
+
+    await client.connect();
+
+    const username = 'canino_conductor';
+    const password = 'holabenas';
+
+    const result = await client.db("todofutbol").collection("users").findOne({ username: username, password: password });
+    console.log(result);
+
+        if (result) {
+            alert("Inicio de sesion correcto");
+            location.reload();
+            console.log("correcto");
+        } else {
+            error.style.opacity=1;
+            console.log("incorrecto");
+        }
+    
+}
 
 boton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = formulario.username.value;
-    const password = formulario.password.value;
 
-    if(findByUsernameAndPassword(client,username,password)){
+    main();
+
+})
+
+
+
+//boton.addEventListener("click", (e) => {
+    //e.preventDefault();
+    
+    
+
+    /*if(findByUsernameAndPassword(client, username, password)){
         alert("Inicio de sesion correcto");
         location.reload();
     } else {
         error.style.opacity=1;
-    }
+    }*/
 
     /*if(username==="usuario" && password==="pas"){
         alert("Inicio de sesion correcto");
@@ -25,4 +59,4 @@ boton.addEventListener("click", (e) => {
     } else {
         error.style.opacity=1;
     }*/
-})
+//})
