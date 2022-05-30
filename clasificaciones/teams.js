13795acf676d1c04904178c3c177a6dc17b0beef
pablosanchezvecino <pablosanchezvecino@uma.es>
2022-05-30 09:11:25 +0200

    Pequeños cambios

--- clasificaciones/teams.js
+++ clasificaciones/teams.js
@@ -2,8 +2,8 @@ const script_tag = document.getElementById('table-loader')
 const competition = script_tag.getAttribute("competition");
 
 fetch('http://api.football-data.org/v2/competitions/' +
-       competition +
-      '/standings', {
+    competition +
+    '/standings', {
     method: 'GET',
     headers: {
         'X-Auth-Token': '68ce06e3eae1416ab29dd79b83831cc8'
@@ -12,15 +12,54 @@ fetch('http://api.football-data.org/v2/competitions/' +
     .then(promesaFetch => promesaFetch.json())
     .then(equipos => {
         equipos.standings[0].table.forEach(equipo => {
-            const style = 'text-align: center; vertical-align: middle;'
+            
             const fila = document.getElementById('equipos').appendChild(document.createElement('tr'))
 
             const posicion = fila.appendChild(document.createElement('th'))
-            posicion.style = style
+            posicion.style = 'text-align: center; vertical-align: middle;'
             posicion.scope = 'row'
             posicion.textContent = equipo.position
 
-            const escudo = fila.appendChild(document.createElement('td'))
+
+
+            const celda = fila.insertCell()
+            celda.style = 'text-align: center; vertical-align: middle;'
+
+            const escudo = celda.appendChild(document.createElement('img'))
+            escudo.src = equipo.team.crestUrl
+            escudo.alt = 'Escudo del ' + equipo.team.name
+            escudo.height = 32
+            const nombre = celda.appendChild(document.createElement('b'))
+            nombre.classList = 'ms-2'
+            nombre.style = 'text-align: center; vertical-align: middle;'
+            nombre.textContent = equipo.team.name
+
+            const puntos = fila.insertCell()
+            puntos.style = 'text-align: center; vertical-align: middle;'
+            puntos.textContent = equipo.points
+            
+            const partidosJugados = fila.insertCell()
+            partidosJugados.style = 'text-align: center; vertical-align: middle;'
+            partidosJugados.textContent = equipo.playedGames
+
+            const partidosGanados = fila.appendChild(document.createElement('td'))
+            partidosGanados.style = 'text-align: center; vertical-align: middle;'
+            partidosGanados.textContent = equipo.won
+
+            const partidosEmpatados = fila.appendChild(document.createElement('td'))
+            partidosEmpatados.style = 'text-align: center; vertical-align: middle;'
+            partidosEmpatados.textContent = equipo.draw
+
+            const partidosPerdidos = fila.appendChild(document.createElement('td'))
+            partidosPerdidos.style = 'text-align: center; vertical-align: middle;'
+            partidosPerdidos.textContent = equipo.lost
+
+            const diferenciaGoles = fila.insertCell()
+            diferenciaGoles.style = 'text-align: center; vertical-align: middle;'
+            diferenciaGoles.textContent = equipo.goalDifference
+            
+            
+            /*const escudo = fila.appendChild(document.createElement('td'))
             escudo.style = style
             const imagen = document.createElement('img')
             imagen.src = equipo.team.crestUrl
@@ -29,7 +68,9 @@ fetch('http://api.football-data.org/v2/competitions/' +
             escudo.appendChild(imagen)
 
             const nombreEquipo = escudo.appendChild(document.createElement('div'))
-            nombreEquipo.textContent= equipo.team.name
+            
+            const textoNombreEquipo = nombreEquipo.appendChild('b');
+            textoNombreEquipo.textContent = equipo.team.name
 
             const puntos = fila.appendChild(document.createElement('td'))
             puntos.style = style
@@ -53,7 +94,7 @@ fetch('http://api.football-data.org/v2/competitions/' +
 
             const diferencia = fila.appendChild(document.createElement('td'))
             diferencia.style = style
-            diferencia.textContent = equipo.goalDifference
+            diferencia.textContent = equipo.goalDifference*/
         });
         document.getElementById("spinner").remove()
     })
---
+++ mongo.js
@@ -0,0 +1,117 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://pablo:1234@cluster0.m2uj4.mongodb.net/test?retryWrites=true&w=majority";
+
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+        // Login
+        await findByUsernameAndPassword(client, 'paco_RMCF', '1234')
+
+        // Registro
+        /*await createListing(client,
+            {
+                username: "canino_conductor",
+                password: "holabuenas",
+                favoriteTeams: []
+            }
+        );*/
+
+        // Añadir equipo a favoritos
+        //await addFavorite(client, 'canino_conductor', '99')
+
+        // Eliminar equipo de favoritos
+        await deleteFavorite(client, 'canino_conductor', '99')
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function findByUsernameAndPassword(client, username, password) {
+    const result = await client.db("todofutbol").collection("users").findOne({ username: username, password: password });
+
+    if (result) {
+        console.log('Login correcto');
+        console.log(result);
+        // Aquí se iniciaría sesión
+    } else {
+        console.log('No se encontró el par (usuario, contraseña)');
+        // Aquí seguiríamos en el formulario e informamos 
+    }
+}
+
+async function createListing(client, newListing) {
+    const result = await client.db("todofutbol").collection("users").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function addFavorite(client, username, newFavorite) {
+    const result = await client.db("todofutbol").collection("users")
+        .updateOne({ username: username }, {
+            $push: {
+                favoriteTeams: newFavorite
+            }
+        });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+async function deleteFavorite(client, username, favorite) {
+    const result = await client.db("todofutbol").collection("users")
+        .updateOne({ username: username }, {
+            $pull: {
+                favoriteTeams: favorite
+            }
+        });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+
